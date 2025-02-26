bf7ec05aac57dadeb588afad335107b79258edaf
toto <50374594+toine08@users.noreply.github.com>
2025-02-26 10:35:43 +0000

    Add site,base and output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://toine08.github.io',
+    base: '/togiblog/',
+    output: 'static',
 });
